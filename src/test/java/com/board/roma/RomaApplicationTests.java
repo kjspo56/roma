ef393e074b8c04b2e438730d7d7348bd16a3799b
tedkim <ted.kim@i-on.net>
2022-10-12 17:13:12 +0900

    Initial commit

---
+++ src/test/java/com/board/roma/RomaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.roma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
